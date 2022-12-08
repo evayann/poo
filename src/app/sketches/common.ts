b9b321ac55be95e617368d10449fd3669fa7d882
evayann <37276188+evayann@users.noreply.github.com>
2022-12-08 15:50:18 +0100

    Grid of rotating square

--- src/app/sketches/common.ts
+++ src/app/sketches/common.ts
@@ -470,4 +470,51 @@ export const sketches = [
       }
     }
   `,
+  ({ primary, secondary, tertiary, bgColor, shapeColor }) => `
+  @grid: 19 / 100%;
+
+  ${shape(shapeColor, bgColor)}
+
+  border: 1px solid #1b1b1c;
+  transform-origin: center;
+  
+  --rotation-delay: calc(((@abs(@abs(@row - @size-row / 1.75) + @abs(@col - @size-col / 2) - @size-col) / @size-col) - 1) * -1s);  
+
+  animation: scale ease-in 3s var(--rotation-delay) infinite alternate;
+
+  @keyframes scale {
+    0%, 100% {
+      rotate: 0.1turn;
+      background: hsl(0.1turn, 54%, 60%);
+    }
+    18% { 
+      rotate: -0.2turn;
+      background: hsl(-0.2turn, 54%, 60%);
+    }
+    38% { 
+      rotate: 0.5turn;
+      background: hsl(0.5turn, 54%, 60%);
+    }
+    42% { 
+      rotate: 1.1turn;
+      background: hsl(1.1turn, 54%, 60%);
+    }
+    58% { 
+      rotate: 0.8turn;
+      background: hsl(0.8, 54%, 60%);
+    }
+    68% { 
+      rotate: 0.6turn;
+      background: hsl(0.6turn, 54%, 60%);
+    }
+    78% { 
+      rotate: 1.01turn;
+      background: hsl(1.01turn, 54%, 60%);
+    }
+    95% {
+      rotate: 0.5turn;
+      background: hsl(0.5turn, 54%, 60%);
+    }
+  }
+  `,
 ];
